9e107836895356707df0f7e77259c97fecb235ea
stephenonchieku1 <onchiekustephen901@gmail.com>
2023-05-02 15:39:09 +0300

    added componrnts folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,26 @@ module.exports = {
     content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
     theme: {
       extend: {
-        
-      },
+         colors:{
+        primary : "#312544",
+        btncolor : "#A162CF",
+        text : "#FFFFFF",
+        dark :  "#0E0E0E",
+        text2 : "#ef5036",
+        bright : "#EF5036 ",
+        gradient1: '#CE6D88',
+        cardColor: '#191919',
+      
+      }
+    },
+    // Configurations for responsiveness
+    screens :{
+      sm : '640px',
+      md:'768px',
+      lg: '1024px',
+      xl: '1280px',
+    }
+    
     },
     plugins: [],
   }
