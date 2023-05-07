14298650c44e36dcfedf2682924178aeb0bf6497
stephenonchieku1 <onchiekustephen901@gmail.com>
2023-05-08 00:54:18 +0300

    configuring tailwind files

---
+++ src/components/navbar.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function navbar() {
+  return (
+    <View>
+      <Text>navbar</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
