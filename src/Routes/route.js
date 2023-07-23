720853d2f2c04a60cd611b535128ac5ad64b46d4
stephenonchieku1 <onchiekustephen901@gmail.com>
2023-07-23 13:09:08 +0300

    added contact routes

--- src/Routes/route.js
+++ src/Routes/route.js
@@ -2,11 +2,13 @@ import React from 'react'
 import{ BrowserRouter,Route,Routes} from 'react-router-dom'
 import  Login from '../components/login'
 import SignIn from '../components/SignIn'
+import Contact from '../components/Contacts'
 
  const route = ()=>{
         return (
           <BrowserRouter>
           <Routes>
+            <Route  path='/' element={<Contact/>} ></Route>
               <Route path='/login' element={<Login/>}></Route>
               <Route path='/register' element={<SignIn/>}></Route>
                
