5ea9d5974af2168e978b31e070368e6e7f4aeb32
stephenonchieku1 <onchiekustephen901@gmail.com>
2023-07-23 13:21:14 +0300

    created contact form

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -4,7 +4,17 @@ import './Contact.css'
 function Contacts() {
   return (
     <section id='contact'>
-
+             <h5>Get In Touch</h5>
+             <h2>Contact Me</h2>
+             <div className='container contact_container'>
+                <div className='contact_options'>
+                   <article className='contact_option'>
+                    
+                   </article>
+                </div>
+             
+                <form action="2"></form>
+             </div>
     </section>
 
   )
